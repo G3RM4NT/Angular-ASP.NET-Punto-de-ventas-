efbdb8b1a17d62e346ece3c420dadcd0a4bd23cf
G3RM4NT <gonzagerman924@gmail.com>
2026-03-18 00:46:01 -0300

    Dia 2 capas y configuracion de DAL

---
+++ APISYSTEM/SYSTEMSALE.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+namespace SYSTEMSALE.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        Task<List<TModel>> Crear(Expression<Func<TModel, bool>> filtro);
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eliminar(TModel modelo);
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null);
+    }
+}
---
+++ APISYSTEM/SYSTEMSALE.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SYSTEMSALE.DAL.Repositorios.Contrato;
+using SYSTEMSALE.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace SYSTEMSALE.DAL.Repositorios
+{
+    public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
+    {
+        private readonly DatabaseventaContext _dbContext;
+
+        public GenericRepository(DatabaseventaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
